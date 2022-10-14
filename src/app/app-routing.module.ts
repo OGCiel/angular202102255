787c6e6675147295c75c94a9647bf78955f87834
OGCiel <piscesmesh14@gmail.com>
2022-10-14 14:34:07 +0700

    4rd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: "dashboard", component: DashboardComponent },
+  { path: "admin", component: AdminComponent },
   { path: "login", component: LoginComponent },
   { path: "signup", component: SignupComponent },
   { path: "", redirectTo: "login", pathMatch: "full" }
